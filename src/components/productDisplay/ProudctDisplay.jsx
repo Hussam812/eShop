55f5ad63e9a713c6deed0878a6bd63708bfc07e2
Hussam812 <hossamhosnihussien@gmail.com>
2023-10-24 22:27:10 +0200

    product page

---
+++ src/components/productDisplay/ProudctDisplay.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import start_icon from "../assets/star_icon.png";
+import start_dull_icon from "../assets/star_dull_icon.png";
+
+const ProudctDisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="proudctdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="proudctdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-start">
+          <img src={start_icon} alt="" />
+          <img src={start_icon} alt="" />
+          <img src={start_icon} alt="" />
+          <img src={start_icon} alt="" />
+          <img src={start_dull_icon} alt="" />
+          <p>{122}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProudctDisplay;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useParams } from "react-router-dom";
+import BreadCrums from "../components/breadCrums/BreadCrums";
+import ProudctDisplay from "../components/productDisplay/ProudctDisplay";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const proudct = all_product.find((e) => e.id === Number(productId));
+  //console.log(proudct);
+
+  return (
+    <div>
+      <BreadCrums product={proudct} />
+      <ProudctDisplay product={proudct} />
+    </div>
+  );
 };
 
 export default Product;
